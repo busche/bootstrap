546405e779e80f79c968ec60698face9cbce3f46
Andre Busche <Andre Busche@147.172.223.27>
2013-08-17 14:00:46 +0200

    v.1.1.3: Added Maven-pom updated example implementation added TODOs

--- src/examples/de/ismll/bootstrap/examples/PojoA.java
+++ src/examples/de/ismll/bootstrap/examples/PojoA.java
@@ -1,5 +1,9 @@
 package de.ismll.bootstrap.examples;
 
+import java.util.Arrays;
+import java.util.Map;
+
+import de.ismll.bootstrap.CommandLineParser;
 import de.ismll.bootstrap.Parameter;
 
 public class PojoA implements Runnable {
@@ -25,10 +29,25 @@ public class PojoA implements Runnable {
 	public void run() {
 		System.out.println("Age is:  " + getAge());
 		System.out.println("Name is: " + getName());
+		System.out.println("Hobbies: " + Arrays.toString(hobbies));
 	}
 
 	public String[] getHobbies() { return hobbies; }
 
 	public void setHobbies(String[] hobbies) { this.hobbies = hobbies; }
+	
+	public static PojoA convert(Object in) {
+		String s = (String) CommandLineParser.convert(in, String.class);
+		
+		// split string
+		Map<String, Object> map = CommandLineParser.parseMap(s, ',', '=');
+		PojoA ret = new PojoA();
+		
+		// set properties
+		CommandLineParser.applyArguments(ret, map);
+		
+		return ret;
+	}
+
 	
 }
