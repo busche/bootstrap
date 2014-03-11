12186fed18991b33ad823062c4cf6475f84abed0
André Busche <andre.busche@gmx.de>
2014-03-11 15:04:37 +0100

    added documentation and a novel constructor

--- src/java/de/ismll/bootstrap/InvalidConversionFormatException.java
+++ src/java/de/ismll/bootstrap/InvalidConversionFormatException.java
@@ -1,16 +1,41 @@
 package de.ismll.bootstrap;
 
+import de.ismll.console.Generic;
+
+/**
+ * An exception denoting an invalid conversion trial.
+ * 
+ * This exception is and should be thrown of a datatype conversion using the
+ * bootstrap framework fails. It is implicitly caught by {@link Generic} and
+ * prints a nicely formatted error message to the console when thrown.
+ * 
+ * 
+ * @author André Busche
+ */
 public class InvalidConversionFormatException extends BootstrapException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -192056817488785045L;
 	private Class<?> sourceType;
 	private Class<?> targetType;
 	private Object sourceString;
 	private Object expectedPattern;
-	
-	public InvalidConversionFormatException(Object bootstrapEnabledObject, Object sourceString, Object expectedPattern) {
+
+	public InvalidConversionFormatException(Object bootstrapEnabledObject,
+			Object sourceString, Object expectedPattern) {
 		super(bootstrapEnabledObject);
 		this.sourceString = sourceString;
 		this.expectedPattern = expectedPattern;
+	}
+
+	public InvalidConversionFormatException(Object bootstrapEnabledObject,
+			Object sourceString, Object expectedPattern, Class<?> sourceType,
+			Class<?> targetType) {
+		this(bootstrapEnabledObject, sourceString, expectedPattern);
+		this.sourceType = sourceType;
+		this.targetType = targetType;
 	}
 	
 	private String msg;
