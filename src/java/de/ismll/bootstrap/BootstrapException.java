1750590fc9f6cc9e9bbfabae3ec9335f97e2df97
André Busche <andre.busche@gmx.de>
2014-03-11 15:12:57 +0100

    added documentation

--- src/java/de/ismll/bootstrap/BootstrapException.java
+++ src/java/de/ismll/bootstrap/BootstrapException.java
@@ -1,5 +1,14 @@
 package de.ismll.bootstrap;
 
+/**
+ * An Exception (superclass) thrown by all Bootstrap-related methods is an unhandled situation occurs.
+ * 
+ * This Exception class inherits all superclass constructors, but should preferably be instantiated using {@link BootstrapException#BootstrapException(Object)} as this is caught and nicely printed by the {@link Generic} launcher class.  
+ * 
+ * 
+ * @author André Busche
+ *
+ */
 public class BootstrapException extends RuntimeException {
 
 	/**
@@ -7,18 +16,15 @@ public class BootstrapException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = -8215318662956331221L;
 
+	protected Object bootstrapEnabledObject;
+
 	public BootstrapException() {
 		super();
 	}
 
-	public BootstrapException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		
-	}
-
-	public BootstrapException(String message, Throwable cause) {
-		super(message, cause);
+	public BootstrapException(Object bootstrapEnabledObject) {
+		super();
+		this.bootstrapEnabledObject = bootstrapEnabledObject;
 		
 	}
 
@@ -27,28 +33,31 @@ public class BootstrapException extends RuntimeException {
 		
 	}
 
+	public BootstrapException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+	
+	public BootstrapException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		
+	}
+
+	public BootstrapException(Throwable cause) {
+		super(cause);
+		
+	}
+	
+	public Object getBootstrapEnabledObject() {
+		return bootstrapEnabledObject;
+	}
+
 	@Override
 	public String getMessage() {
 		if (bootstrapEnabledObject==null) return super.getMessage();
 		
 		return "Bootstrap Exception while working on an object of type " + bootstrapEnabledObject.getClass().getCanonicalName() + ": " + super.getMessage();
-	}
-	
-	public BootstrapException(Throwable cause) {
-		super(cause);
-		
-	}
-
-	protected Object bootstrapEnabledObject;
-	
-	public BootstrapException(Object bootstrapEnabledObject) {
-		super();
-		this.bootstrapEnabledObject = bootstrapEnabledObject;
-		
-	}
-
-	public Object getBootstrapEnabledObject() {
-		return bootstrapEnabledObject;
 	}
 
 	public void setBootstrapEnabledObject(Object bootstrapEnabledObject) {
