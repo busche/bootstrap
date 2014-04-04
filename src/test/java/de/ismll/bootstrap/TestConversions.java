daf28cf33cd33eb0f3e406f43c50c623566ada9d
André Busche <andre.busche@gmx.de>
2014-04-04 14:47:47 +0200

    removed even more FundBugs bugs

--- src/test/java/de/ismll/bootstrap/TestConversions.java
+++ src/test/java/de/ismll/bootstrap/TestConversions.java
@@ -4,7 +4,9 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.Random;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import de.ismll.bootstrap.oo.AbstractA;
@@ -17,10 +19,22 @@ import de.ismll.bootstrap.oo.AbstractA;
  */
 public class TestConversions {
 
+	private Random r;
 
+	@Before
+	public void setUp() throws Exception {
+		r = new Random();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		r=null;
+	}
+
+	
 	@Test
 	public void testChar(){
-		char value = (char) (Math.random()*254);
+		char value = (char) (r.nextInt(254));
 		String valueAsString = "" + value;
 		Character conversion = (Character) CommandLineParser.convert(valueAsString, Character.class);
 		Assert.assertEquals(value, conversion.charValue());
@@ -29,11 +43,11 @@ public class TestConversions {
 	@Test
 	// FIXME: There is a bug in this test. However - it could not be identified on 2014-03-25 when fixing Math.random() to the extremes.
 	public void testCharArray(){
-		byte max = 127-33;
+		byte max = 52;
 		char[] value = {
-				(char) (33 +Math.random()*max),
-				(char) (33 +Math.random()*max),
-				(char) (33 +Math.random()*max),
+				(char) (33 +r.nextDouble()*max),
+				(char) (33 +r.nextDouble()*max),
+				(char) (33 +r.nextDouble()*max),
 		};
 		String valueAsString = Arrays.toString(value);
 		char[] conversion = (char[]) CommandLineParser.convert(valueAsString, char[].class);
@@ -87,7 +101,7 @@ public class TestConversions {
 	
 	@Test
 	public void testBoolean(){
-		Boolean value = Boolean.valueOf(new Random().nextBoolean());
+		Boolean value = Boolean.valueOf(r.nextBoolean());
 		String valueAsString = value.toString();
 		Boolean conversion = (Boolean) CommandLineParser.convert(valueAsString, Boolean.class);
 		Assert.assertEquals(value, conversion);
@@ -96,10 +110,10 @@ public class TestConversions {
 	@Test
 	public void testBooleanArray(){
 		boolean[] value = {
-				(new Random().nextBoolean()),
-				(new Random().nextBoolean()),
-				(new Random().nextBoolean()),
-				(new Random().nextBoolean())
+				(r.nextBoolean()),
+				(r.nextBoolean()),
+				(r.nextBoolean()),
+				(r.nextBoolean())
 		};
 		String valueAsString = Arrays.toString(value);
 		boolean[] conversion = (boolean[]) CommandLineParser.convert(valueAsString, boolean[].class);
@@ -111,7 +125,7 @@ public class TestConversions {
 
 	@Test
 	public void testFloat(){
-		float value = (float) (Math.random()*10000);
+		float value = (float) (r.nextFloat()*10000);
 		String valueAsString = "" + value;
 		Float conversion = (Float) CommandLineParser.convert(valueAsString, Float.class);
 		Assert.assertEquals(value, conversion.floatValue(), 0.00001);
@@ -120,9 +134,9 @@ public class TestConversions {
 	@Test
 	public void testFloatArray(){
 		float[] value = {
-				(float) (Math.random()*254),
-				(float) (Math.random()*254),
-				(float) (Math.random()*254),
+				(float) (r.nextFloat()*254),
+				(float) (r.nextFloat()*254),
+				(float) (r.nextFloat()*254),
 		};
 		String valueAsString = Arrays.toString(value);
 		float[] conversion = (float[]) CommandLineParser.convert(valueAsString, float[].class);
@@ -134,7 +148,7 @@ public class TestConversions {
 
 	@Test
 	public void testDouble(){
-		double value = Math.random()*10000;
+		double value = r.nextDouble()*10000;
 		String valueAsString = "" + value;
 		Double conversion = (Double) CommandLineParser.convert(valueAsString, Double.class);
 		Assert.assertEquals(value, conversion.doubleValue(), 0.00001);
@@ -143,9 +157,9 @@ public class TestConversions {
 	@Test
 	public void testDoubleArray(){
 		double[] value = {
-				Math.random()*254,
-				Math.random()*254,
-				Math.random()*254,
+				r.nextDouble()*254,
+				r.nextDouble()*254,
+				r.nextDouble()*254,
 		};
 		String valueAsString = Arrays.toString(value);
 		double[] conversion = (double[]) CommandLineParser.convert(valueAsString, double[].class);
@@ -157,7 +171,7 @@ public class TestConversions {
 
 	@Test
 	public void testShort(){
-		short value = (short) (Math.random()*100000);
+		short value = (short) (r.nextInt(100000));
 		String valueAsString = "" + value;
 		Short conversion = (Short) CommandLineParser.convert(valueAsString, Short.class);
 		Assert.assertEquals(value, conversion.shortValue());
@@ -166,9 +180,9 @@ public class TestConversions {
 	@Test
 	public void testShortArray(){
 		short[] value = {
-				(short) (Math.random()*10000000),
-				(short) (Math.random()*10000000),
-				(short) (Math.random()*10000000),
+				(short) (r.nextInt(10000000)),
+				(short) (r.nextInt(10000000)),
+				(short) (r.nextInt(10000000)),
 		};
 		String valueAsString = Arrays.toString(value);
 		short[] conversion = (short[]) CommandLineParser.convert(valueAsString, short[].class);
@@ -180,7 +194,7 @@ public class TestConversions {
 
 	@Test
 	public void testInt(){
-		int value = (int) (Math.random()*10000000);
+		int value = r.nextInt(10000000);
 		String valueAsString = "" + value;
 		Integer conversion = (Integer) CommandLineParser.convert(valueAsString, Integer.class);
 		Assert.assertEquals(value, conversion.intValue());
@@ -189,13 +203,12 @@ public class TestConversions {
 	@Test
 	public void testIntArray(){
 		int[] value = {
-				(int) (Math.random()*10000000),
-				(int) (Math.random()*10000000),
-				(int) (Math.random()*10000000),
-				(int) (Math.random()*10000000),
-				(int) (Math.random()*10000000),
-				(int) (Math.random()*10000000),
-				(int) (Math.random()*10000000),
+				r.nextInt(10000000),
+				r.nextInt(10000000),
+				r.nextInt(10000000),
+				r.nextInt(10000000),
+				r.nextInt(10000000),
+				r.nextInt(10000000),
 		};
 		String valueAsString = Arrays.toString(value);
 		int[] conversion = (int[]) CommandLineParser.convert(valueAsString, int[].class);
@@ -207,7 +220,7 @@ public class TestConversions {
 
 	@Test
 	public void testLong(){
-		long value = (long) (Math.random()*10000000);
+		long value = (long) (r.nextDouble()*10000000);
 		String valueAsString = "" + value;
 		Long conversion = (Long) CommandLineParser.convert(valueAsString, Long.class);
 		Assert.assertEquals(value, conversion.longValue());
@@ -216,8 +229,8 @@ public class TestConversions {
 	@Test
 	public void testLongArray(){
 		long[] value = {
-				(long) (Math.random()*10000000),
-				(long) (Math.random()*10000000),
+				(long) (r.nextDouble()*10000000),
+				(long) (r.nextDouble()*10000000),
 		
 		};
 		String valueAsString = Arrays.toString(value);
